f75a06799be2f7eb226d74579d0fe9662e20f3a1
Rohan Laxman Gadge <rohangadge486@gmail.com>
2026-02-14 18:14:17 +0530

    Create Move_Zeros.java
    code done moveing zros last

---
+++ Arrays/day/one/Move_Zeros.java
@@ -0,0 +1,37 @@
+public class Move_Zeros {
+
+	public static void main(String[] args) {
+		 int[] arr = {0, 5,0,6,8,9,3,0,45};
+		 
+		 
+//		int i=0;
+//		for(int j=1;j<arr.length;j++) {
+//			
+//			if(arr[i]!=0) {
+//				i++;
+//			}else if(arr[i]==0 && arr[j]!=0) {
+//				int temp=arr[i];
+//				arr[i]=arr[j];
+//				arr[j]=temp;
+//				i++;
+//			}
+//		}
+//		
+		 
+		 int i = 0;
+		 for (int j = 0; j < arr.length; j++) {
+		     if (arr[j] != 0) {
+		      
+		         int temp = arr[i];
+		         arr[i] = arr[j];
+		         arr[j] = temp;
+		         i++;
+		     }
+		 }
+		for(i=0;i<arr.length;i++) {
+     	   System.out.print(arr[i]+" ");
+        }
+
+	}
+
+}
